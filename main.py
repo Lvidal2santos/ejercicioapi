1fd2995319d736b1aec59afd6c6594ec0dd2eb5a
Lvidal2santos <lucasvidaldossantos@gmail.com>
2021-10-05 23:11:29 -0300

    refactored endpoint

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+import requests
 
-CAMBIOTODAYAPIKEY = '11155|CLin3h*eomdP*quJH7eUXXGrJ2ucmSxd'
+TOKEN = '11155|CLin3h*eomdP*quJH7eUXXGrJ2ucmSxd'
 
 app = FastAPI()
 
 
-@app.get("/")
-def hello():
-    return {"message": "Testing FastAPI"}
+@app.get("/cotizacion/{currency}/{quantity}")
+def cotizacion(currency: str = Path(None, description="El código ISO de la moneda que se quiere cotizar"),
+               quantity: int = Path(None, description="la cantidad que se quiere cotizar.")):
+
+    return requests.get(f'http://api.wahrungsrechner.org/v1/quotes/THB/{currency}?quantity={quantity}&key={TOKEN}')
+
