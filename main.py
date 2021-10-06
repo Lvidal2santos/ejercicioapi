154866d061b2b175618a3938b3e79a0a8b21386d
Lvidal2santos <lucasvidaldossantos@gmail.com>
2021-10-05 23:39:16 -0300

    modify the target url

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ app = FastAPI()
 def cotizacion(currency: str = Path(None, description="El código ISO de la moneda que se quiere cotizar"),
                quantity: int = Path(None, description="la cantidad que se quiere cotizar.")):
 
-    return requests.get(f'http://api.wahrungsrechner.org/v1/quotes/THB/{currency}?quantity={quantity}&key={TOKEN}')
+    return requests.get(f'https://api.cambio.today/v1/quotes/ARS/{currency}/json?quantity={quantity}&key=11155|CLin3h*eomdP*quJH7eUXXGrJ2ucmSxd')
 
