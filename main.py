a4223e9bd69ca5ddacbf52459fb6f2f61f4b5bd2
Lvidal2santos <lucasvidaldossantos@gmail.com>
2021-10-06 00:36:53 -0300

    fix request

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 import requests
 
 TOKEN = '11155|CLin3h*eomdP*quJH7eUXXGrJ2ucmSxd'
-
+VALID_CURRENCIES = ['BGN', 'BRL', 'USD', 'EUR']
 app = FastAPI()
 
 
 @app.get("/cotizacion/{currency}/{quantity}")
 def cotizacion(currency: str = Path(None, description="El código ISO de la moneda que se quiere cotizar"),
-               quantity: int = Path(None, description="la cantidad que se quiere cotizar.")):
+               quantity: str = Path(None, description="La cantidad que se quiere cotizar.")):
 
-    return requests.get(f'https://api.cambio.today/v1/quotes/ARS/{currency}/json?quantity={quantity}&key=11155|CLin3h*eomdP*quJH7eUXXGrJ2ucmSxd')
+    currency = currency.upper()
+    if currency not in VALID_CURRENCIES:
+        return HTTPException(status_code=404, detail="Invalid currency ISO")
+
+    response = requests.get(f'https://api.cambio.today/v1/quotes/ARS/{currency}/json?quantity={quantity}&key={TOKEN}')
+
+    return response.json()['result']['amount']
 
